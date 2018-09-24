cada67a69df7a86d31206b4605fde477556ba90a
kneilson <kneilson@student.mamkschools.org>
2018-09-24 09:41:59 -0400

    procedural decomposition work in progress

--- src/Ch7Arrays/WeatherAnalysis.java
+++ src/Ch7Arrays/WeatherAnalysis.java
@@ -6,37 +6,56 @@ package Ch7Arrays;
 import java.util.Scanner;
 
 public class WeatherAnalysis {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        //Step1: ask for user input, # of days
+        numOfDays();
+    }
+
+    //Step1: ask for user input, # of days
+    public static void numOfDays() {
         Scanner console = new Scanner(System.in);
         System.out.print("How many days' temperatures? ");
         int numdays = console.nextInt();
+        createArray(console, numdays);
 
-        //Step2: use user input to create an container
-        int[] tempArray = new int[numdays];
-        int cumSum = 0;
-
-        //Step3: populate the container with user input, Temps
-        for(int i = 0; i < numdays; i++){
-            System.out.print("Day " + (i+1) + "'s high temp: ");
-            tempArray[i] = console.nextInt();
-            cumSum+=  tempArray[i];
-        }
-
-        //Step4: determine the average Temp
-        double average = (double)cumSum / (double)numdays;
-        average = Math.round(average * 10.0) / 10.0;
-        System.out.println("Average temp = " + average);
-
-        //Step5: determine the # of days above the average Temp
-        int aboveAverage = 0;
-        for(int i = 0; i < numdays; i++) {
-            if(tempArray[i] > average){
-                aboveAverage++;
-            }
-        }
-        System.out.println(aboveAverage + " days were above average.");
     }
 
+    //Step2: use user input to create an container
+    public static void createArray(Scanner console, int numdays) {
+        int[] tempArray = new int[numdays];
+        int cumSum = 0;
+        pupulateArray(numdays);
+    }
+
+    //Step3: populate the container with user input, Temps
+    public static void
+        for(
+    int i = 0;
+    i<numdays;i++)
+
+    {
+        System.out.print("Day " + (i + 1) + "'s high temp: ");
+        tempArray[i] = console.nextInt();
+        cumSum += tempArray[i];
+    }
+
+    //Step4: determine the average Temp
+    double average = (double) cumSum / (double) numdays;
+    average =Math.round(average *10.0)/10.0;
+        System.out.println("Average temp = "+average);
+
+    //Step5: determine the # of days above the average Temp
+    int aboveAverage = 0;
+        for(
+    int i = 0;
+    i<numdays;i++)
+
+    {
+        if (tempArray[i] > average) {
+            aboveAverage++;
+        }
+    }
+        System.out.println(aboveAverage +" days were above average.");
 }
+
+
