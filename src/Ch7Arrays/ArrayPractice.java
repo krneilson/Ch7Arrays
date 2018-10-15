f4754d2716adaff34a5e45e5738aa2b537205914
kneilson <kneilson@student.mamkschools.org>
2018-10-15 09:56:45 -0400

    mode start

--- src/Ch7Arrays/ArrayPractice.java
+++ src/Ch7Arrays/ArrayPractice.java
@@ -3,28 +3,36 @@ package Ch7Arrays;
 import java.util.Arrays;
 
 public class ArrayPractice {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //Step1: declare the variable
         int[] numbers1 = new int[5]; //an integer array of size 5
         //Step2: initialize the variable
         System.out.println(Arrays.toString(numbers1)); //before populating the array
-        for(int i = 0; i < 5; i++){
-            numbers1[i] = i+1;
+        for (int i = 0; i < 5; i++) {
+            numbers1[i] = i + 1;
         }
         System.out.println(Arrays.toString(numbers1)); // after populating the array
 
         //Step1&2: declare and initialize
-        int[] numbers2 = {1,2,3,4,5};
+        int[] numbers2 = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers2));
         randomArray();
+        int num = 669360267;
+
+        System.out.println("Mode is: " + mode(num));
+
     }
 
-    public static void randomArray(){
-        int randomNum = (int)(Math.random() * 10 + 1);
+    public static void randomArray() {
+        int randomNum = (int) (Math.random() * 10 + 1);
         int[] ranArray = new int[randomNum];
-        for(int i = 0; i < ranArray.length; i++){
-            ranArray[i] = (int)(Math.random() * 10 + 1);
+        for (int i = 0; i < ranArray.length; i++) {
+            ranArray[i] = (int) (Math.random() * 10 + 1);
         }
         System.out.println(Arrays.toString(ranArray));
+    }
+
+    public static int mode(int num){
+        
     }
 }
