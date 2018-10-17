a5c89196e24e794b94019719d986c9d700e947fc
kneilson <kneilson@student.mamkschools.org>
2018-10-17 09:56:05 -0400

    Mode Class finished

--- src/Ch7Arrays/KimballNeilsonMode.java
+++ src/Ch7Arrays/KimballNeilsonMode.java
@@ -1,12 +1,17 @@
 package Ch7Arrays;
 
+//this program determines the most frequent digit in a number.
 public class KimballNeilsonMode {
     public static void main(String[] args){
+
+        //number can be set to the desired number by commenting line 9 out
         int num = 669360267;
         num = 5713523;
+        //program is using the number from the tie example
         System.out.println("Mode is: " + goodmode(num));
     }
 
+    //this method determines the most frequent digit
     public static int goodmode(int num){
         int[] array = new int[10];
         while(num > 0){
